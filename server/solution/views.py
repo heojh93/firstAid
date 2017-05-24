630ce2031f9d38e0a748ba52a7aab469cb49ddb0
2013011811 <eun104103@hanyang.ac.kr>
2017-05-25 07:29:05 +0900

    ing

--- server/solution/views.py
+++ server/solution/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Textbook
+
 
 # Create your views here.
+
+
+def textbook_list(request):
+	textbooks = Textbook.object.All()
+	return render(request, 'solution/textbook.html', {})
+
+
