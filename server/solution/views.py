1e8f0a157d6d4bcca4c8582629f2c879321f3ad8
2013011811 <eun104103@hanyang.ac.kr>
2017-05-25 14:39:21 +0900

    sample

--- server/solution/views.py
+++ server/solution/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Textbook
 
+import json
 
 # Create your views here.
 
 
 def textbook_list(request):
-	textbooks = Textbook.object.All()
-	return render(request, 'solution/textbook.html', {})
+	textbooks_list = []
+	textbooks = Textbook.objects.all()
+	for textbook in textbooks:
+		t = {}
+		t['title'] = textbook.title
+		t['author'] = textbook.author
+		textbooks_list.append(t)
+	data = json.dumps(textbooks_list)
+	return HttpResponse(data)
+#return render(request, 'solution/textbook.html', {})
 
 
