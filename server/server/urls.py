3c4b2a37a6ed70041a439015854816e317a57a67
2013011811 <eun104103@hanyang.ac.kr>
2017-06-01 16:34:42 +0900

    send data to client by json

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^solution/', include('solution.urls')),
 ]
+
+urlpatterns += static('uploads', document_root=settings.MEDIA_ROOT)
